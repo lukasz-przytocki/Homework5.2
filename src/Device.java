e03c8c424d4df6b96045ee495c8086599e35ed2a
lukasz.przytocki <lukasz.przytoci@aptitudesoftware.com>
2019-05-15 14:07:01 +0200

    Added files related to exercise 5.2

---
+++ src/Device.java
@@ -0,0 +1,20 @@
+public class Device {
+    private String code;
+    private String producer;
+    private String type;
+    private double price;
+
+    public Device(String code, String producer, String type, double price) {
+        this(code, producer, type);
+        this.price = price;
+    }
+
+    public Device(String code, String producer, String type) {
+        this.code = code;
+        this.producer = producer;
+        this.type = type;
+    }
+        public String getInfo(){
+            return "Kod: " + code + ", producent: " + producer + ", typ: " + type + ", cena: " + price;
+        }
+    }
